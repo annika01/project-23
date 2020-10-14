a6ceda87cd769b5c311532ca86d96259d1461b54
annika01 <71920315+annika01@users.noreply.github.com>
2020-10-14 18:05:00 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,16 @@
+class Box {
+    constructor(x,y,w,h){
+        var option= {
+            isStatic: true
+        }
+        this.body= Bodies.rectangle(x,y,w,h, option)
+        World.add(world, this.body)
+        this.w=w 
+        this.h=h
+    }
+    display (){
+rectMode(CENTER)
+fill ("red")
+rect(this.body.position.x, this.body.position.y, this.w, this.h)
+    }
+}
